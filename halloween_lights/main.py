1f422c2cbc76bee5c2a1bff134a7dae71836058d
ctsf1 <colin.tsf@gmail.com>
2021-10-14 11:57:19 +0100

    updated main.py, removed sound output file

--- halloween_lights/main.py
+++ halloween_lights/main.py
@@ -6,11 +6,9 @@ import neopixel
 # --- CONFIG --- #
 tick=0
 TPS = 20
-secondsToDecay = 2
+secondsToDecay = 3
 ticksLeft = 0
 ticksMax = TPS * secondsToDecay
-
-vibrationThreshold = 20
 
 BASE_ORANGE = (90,30,0)   # Base color to be used
 DIFF_ORANGE = (30,10,0)    # Darker Orange
@@ -22,24 +20,15 @@ np = neopixel.NeoPixel(pin0, STRIP_SIZE)
 # function to set the LED RGB colors for the neopixel strip on a given tick (Colin)
 def fillNeopixelRGB(tick):
     for i in range(STRIP_SIZE):
-            np[i] = tuple([round(BASE_ORANGE[j]-COLOR_DIFF[j]*sin(pi*(tick%360)/180+(tick%360)*pi/18)) for j in range(3)])
-    
+        np[i] = tuple([round(BASE_ORANGE[j]-COLOR_DIFF[j]*sin(pi*(tick%360)/180+(tick%360)*pi/18)) for j in range(3)])
     return
-
-# functions to play and stop sound output (Javkhlan)
-def playSoundOutput():
-    return None
-
-def stopSoundOutput():
-    return None
 
 # main loop
 while True:
 
     soundLevel = microphone.sound_level()
 
-    if soundLevel > vibrationThreshold:
-        if ticksLeft == 0: playSoundOutput()
+    if soundLevel:
         ticksLeft = ticksMax
 
     if ticksLeft > 0:
@@ -49,7 +38,6 @@ while True:
         tick+=1; ticksLeft -= 1; np.show()
         if ticksLeft == 0:
             np.clear()
-            stopSoundOutput()
         
     else: np.clear()
     sleep(1000//TPS)
